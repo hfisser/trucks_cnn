39a63c093af35fb449f41fc67e2cbced75beefbd
hfisser <henrik.fisser@t-online.de>
2020-09-11 14:25:51 +0200

    write larger sub tiles

--- labeling/subset_s2_tiles.py
+++ labeling/subset_s2_tiles.py
@@ -1,10 +1,12 @@
 import os
 import numpy as np
 import rasterio
+import geopandas as gpd
+from shapely import geometry
 
 main_dir = "F:\\Masterarbeit\\DLR\\project\\1_cnn_truck_detection\\training_data\\data"
 directories = [os.path.join(main_dir, x) for x in os.listdir(main_dir)]
-subset_size = 36
+subset_size = 5490
 
 
 def subset(d, sub_size):
@@ -18,35 +20,44 @@ def subset(d, sub_size):
         band_names = np.array([f.split("_")[2] for f in files])
         data = []
         kwargs = None
-        for b in ["B02", "B03", "B04"]:
+        bands = ["B04", "B03", "B02"]
+        for b in bands:
             fname = files[np.where(band_names == b)[0][0]]
             with rasterio.open(os.path.join(d2, fname)) as r:
                 data.append(r.read(1))
                 kwargs = r.profile
-        kwargs.update(count=4,
+        dim = range(int(kwargs["height"] / sub_size))
+        kwargs.update(count=len(bands),
                       driver="GTiff",
                       height=sub_size,
                       width=sub_size,
-                      dtype="uint16")
+                      dtype="uint32")
+        transform = kwargs["transform"]
         # create subsets of the data
-        dim = range(sub_size)
         for y in dim:
+            y_low = y * sub_size
+            y_up = (y + 1) * sub_size
             for x in dim:
+                x_low = x * sub_size
+                x_up = (x + 1) * sub_size
                 sub = []
                 for arr in data:
-                    y_up = (y + 1) * sub_size
-                    x_up = (x + 1) * sub_size
-                    sub.append(arr[y * sub_size: y_up, x * sub_size: x_up])
+                    sub.append(arr[y_low:y_up, x * x_low:x_up])
+                x_updated, y_updated = rasterio.transform.xy(transform, x_low, y_low)
+                t = list(transform)
+                kwargs.update(transform=rasterio.Affine(t[0], t[1], x_updated, t[3], t[4], y_updated))
                 sub = np.array(sub)
-                fname = "_".join([os.path.basename(d), "y" + str(y), "x" + str(x)]) + ".tif"
+                fname = "_".join([os.path.basename(d), "y" + str(y_low), "x" + str(x_low)]) + ".tif"
+                # create empty GPKG for labeling
+                gdf = gpd.GeoDataFrame({"index": [1]},
+                                       geometry=[geometry.box(x_updated, y_updated, x_updated + 0.1, y_updated + 0.1)],
+                                       crs=str(kwargs["crs"]))
+                gdf.to_file(os.path.join(dir_out, fname.split(".")[0]+".gpkg"), driver="GPKG")
                 file_out = os.path.join(dir_out, fname)
                 if not os.path.exists(file_out):
-                    # check if there is potential for trucks in the subset
-                    proceed = can_have_trucks(sub[0], sub[1], sub[2])
-                    if proceed:
-                        with rasterio.open(file_out, "w", **kwargs) as target:
-                            for i, band in enumerate(list(sub)):
-                                target.write(band.astype(np.uint16), i+1)
+                    with rasterio.open(file_out, "w", **kwargs) as target:
+                        for i, band in enumerate(list(sub)):
+                            target.write(band.astype(np.uint32), i+1)
 
 
 def can_have_trucks(blue, green, red):
